ca550bc84c0a694b9d288b540f8d6cd54ac6d3e0
husannigmatjonov343-collab <husannigmatjonov343@gmail.com>
2026-07-24 16:59:28 +0500

    update wsgi.py

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,10 +1,13 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings') # loyihangiz nomi 'app' bo'lsa
+# Bu yerda loyiha papkasi 'app' ekani ko'rsatilgan bo'lishi shart:
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
-app = application  # <- Ushbu qator bo'lishi shart
+
+# Vercel talab qiladigan o'zgaruvchi:
+app = application
 
 import os
 import sys
@@ -12,7 +15,5 @@ import sys
 # Python ildiz katalogidagi modullarni topishi uchun yo'lni ko'rsatamiz
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
--- urls.py
+++
@@ -1,9 +0,0 @@
-# ✅ TO'G'RI: Makkah_site/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # Bu yerda ILOVA (APP) nomi bo'lishi kerak!
-    path('', include('sayt_ilovasi.urls')), 
-]
