7ca14c44cff2619be209bde9336da39ac42e820e
husannigmatjonov343-collab <husannigmatjonov343@gmail.com>
2026-07-24 17:10:36 +0500

    Fix: add views.py and fix urls import

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
-# app/urls.py
+from django.http import HttpResponse
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    # Favicon so'rovi kelganida 204 (No Content) qaytarish:
+    path('favicon.ico', lambda request: HttpResponse(status=204)),
 ]
